3e4c534433c459692675f54856fc9b19bd651078
João Lucas <lucasbanana96@gmail.com>
2023-01-30 20:21:45 -0300

    sequelize config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { sequelize } from "./database";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
+    sequelize.authenticate().then(()=>{
+        console.log('DB connection successful');
+    });
     console.log(`Server started successfuly at port ${PORT}`);
 });
 
