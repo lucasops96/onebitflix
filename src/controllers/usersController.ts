a2ace88e93ec267d4458f3792e762c1f37da67ae
João Lucas <lucasbanana96@gmail.com>
2023-03-01 19:22:22 -0300

    Atualizando informações do usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -4,6 +4,40 @@ import { userService } from "../services/userService";
 
 
 export const usersController = {
+    // GET /users/current
+    show: async (req:AuthenticatedRequest, res: Response)=>{
+        try {
+            const currentUser = req.user!
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+    
+    // PUT /users/current
+    update: async(req:AuthenticatedRequest, res:Response)=>{
+        const { id } = req.user
+        const { firstName, lastName, phone, email, birth} = req.body
+
+        try {
+            const updateUser = await userService.update(id, {
+                firstName,
+                lastName,
+                phone,
+                email,
+                birth
+            })
+
+            return res.json(updateUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+    
     // GET /users/current/watching
     watching: async(req:AuthenticatedRequest, res:Response) =>{
         const {id} = req.user!
