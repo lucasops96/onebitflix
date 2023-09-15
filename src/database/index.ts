ec8f0e7baaf5f12d01ffb58e667dd51688db5df2
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-09-14 22:28:29 -0300

    alteração da porta database

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize =  new Sequelize({
     dialect: 'postgres',
     host: 'localhost',
-    port: 5432,
+    port: 5433,
     database: 'onebitflix_development',
     username: 'postgres',
     password: 'eutireium2',
